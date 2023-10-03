388692884d761219062894904977c8e6b1b03af0
Emilio Gajardo <ej.gajardo@gmail.com>
2023-10-03 16:49:06 -0300

    punto de guardado de seccion 6 yes no maybe app fincionalidad

---
+++ lib/presentation/providers/chat_provider.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/config/helpers/get_yes_no_answer.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
+
+class ChatProvider extends ChangeNotifier {
+  final ScrollController chatScrollController = ScrollController();
+
+  final GetYesNoAnswer getYesNoAnswer = GetYesNoAnswer();
+
+  List<Message> messageList = [
+    Message(text: 'Hola', fromWho: FromWho.me),
+    Message(text: '¿Dónde estás?', fromWho: FromWho.me),
+  ];
+
+  Future<void> sendMessage(String text) async {
+    if (text.isEmpty) return;
+    final newMessage = Message(text: text, fromWho: FromWho.me);
+    messageList.add(newMessage);
+
+    if (text.endsWith('?')) {
+      herReply();
+    }
+    notifyListeners();
+    moveScrollToBottom();
+  }
+
+  Future<void> herReply() async {
+    final herMessage = await getYesNoAnswer.getAnswer();
+    messageList.add(herMessage);
+    notifyListeners();
+    moveScrollToBottom();
+  }
+
+  Future<void> moveScrollToBottom() async {
+    await Future.delayed(const Duration(milliseconds: 200));
+    chatScrollController.animateTo(
+        chatScrollController.position.maxScrollExtent, // posicion
+        duration: const Duration(milliseconds: 300), // duracion de animacion
+        curve: Curves.easeOut // que tipo de animacion
+        );
+  }
+}
